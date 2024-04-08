35c6755da0039822f0df6d769ef3af83aa6b7780
hdbhandari <himanshubhandari5@yahoo.co.in>
2024-04-08 12:30:19 +0530

    continue learning

--- 102-hooks/src/Pages/App.js
+++ 102-hooks/src/Pages/App.js
@@ -10,23 +10,21 @@ import UseRefHook from "./UseRefHook"
 import UseReducerHook from "./UseReducerHook"
 import UseLayoutEffectHook from "./UseLayoutEffectHook"
 
-class App extends React.Component {
-  render() {
-    return (<>
-      <Router>
-        <Navbar />
-        <Routes>
-          <Route path="/use-state" element={<UseStateHook />} />
-          <Route path="/use-effect" element={<UseEffectHook />} />
-          <Route path="/use-callback" element={<UseCallbackHook />} />
-          <Route path="/use-memo" element={<UseMemoHook />} />
-          <Route path="/use-ref" element={<UseRefHook />} />
-          <Route path="/use-reducer" element={<UseReducerHook />} />
-          <Route path="/use-layout-effect" element={<UseLayoutEffectHook />} />
-        </Routes>
-      </Router>
-    </>)
-  }
+const App = () => {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/use-state" element={<UseStateHook />} />
+        <Route path="/use-effect" element={<UseEffectHook />} />
+        <Route path="/use-callback" element={<UseCallbackHook />} />
+        <Route path="/use-memo" element={<UseMemoHook />} />
+        <Route path="/use-ref" element={<UseRefHook />} />
+        <Route path="/use-reducer" element={<UseReducerHook />} />
+        <Route path="/use-layout-effect" element={<UseLayoutEffectHook />} />
+      </Routes>
+    </Router>
+  )
 }
 
 export default App
--- readme.md
+++ readme.md
@@ -4,7 +4,7 @@
 
 ## Directories
 
-- 101-basics: React basics
+- 101-basics: React basics with class based components
 - 102-hooks: React inbuilt hooks
 - 103-custom-hooks: React Custom Hooks
 - [TODO] 104-context-api: Did long back, need to check again
